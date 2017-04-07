cb68676ad4020e1ee56c9fade90f80639f282de2
zh99998 <zh99998@gmail.com>
2017-04-07 18:43:37 +0800

    邮件美化

--- views/index.ts
+++ views/index.ts
@@ -3,13 +3,14 @@
  */
 
 import * as fs from 'fs';
+import * as Handlebars from 'handlebars';
 
 function load_template(view: string) {
     const templates = new Map();
 
     // 增加语言要往这里加
-    for (let locale of ['zh-CN']) {
-        templates.set(locale, Handlebars.compile(fs.readFileSync(`views/${view}.${locale}.hbs`)));
+    for (let locale of ['zh-CN', 'en-US']) {
+        templates.set(locale, Handlebars.compile(fs.readFileSync(`views/${view}.${locale}.hbs`, { encoding: 'utf-8' })));
     }
     return ({ locale, ...params }) => {
         return templates.get(locale)(params);
