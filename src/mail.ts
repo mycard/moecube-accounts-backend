0158cc65489500e250e7eed25893eed5c371e7a7
zh99998 <zh99998@gmail.com>
2019-09-16 00:26:39 +0800

    mail allow no auth

--- src/mail.ts
+++ src/mail.ts
@@ -4,7 +4,7 @@ import config from '../config';
 let tp = nodemailer.createTransport({
     host: config.Mail.SMTP_HOST,
     port: config.Mail.SMTP_PORT,
-    auth: {
+    auth: config.Mail.SMTP_PASSWORD && {
         user: config.Mail.SMTP_USERNAME,
         pass: config.Mail.SMTP_PASSWORD
     },
